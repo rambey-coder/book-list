d9702d5924facb9ae66165c1b4916f409cc7238a
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-10-21 13:05:56 +0100

    functionality added

--- src/Component/BookDetails.js
+++ src/Component/BookDetails.js
@@ -2,17 +2,34 @@ import {React, useState, useEffect} from 'react'
 import '../App'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import { BOOK_DETAILS } from '../Api'
 
 const BookDetails = () => {
   const [book, setBook] = useState({})
 
-  useEffect(() => {
-    
-  },[])
-
   const { id } = useParams()
+
+  useEffect(() => {
+    axios.get(`${BOOK_DETAILS}/${id}`)
+          .then(res => setBook(res.data))
+          .catch(err => console.log(err))
+  },[id])
+
   return (
-    <div className='book-details'>BookDetails</div>
+    <div className='book-details'>
+      <div>
+        <h2>{book?.title}</h2>
+        <img src={book?.image_url} alt="" />
+      </div>
+      <div>
+        <h2>Desc</h2>
+        <p>{book?.description}</p>
+        <h2>Authors</h2>
+        <p>{book?.authors}</p>
+        <h2>Genres</h2>
+        <p>{book?.genres}</p>
+      </div>
+    </div>
   )
 }
 
